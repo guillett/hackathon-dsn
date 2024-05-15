581afa2009b4b88ceba10b501da99ff4f61b7c57
cgl <lallemand.chloe@gmail.com>
2024-05-15 10:31:33 +0200

    initialisation survey scenario

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import uuid
 
 import pandas as pd
 
+from openfisca_survey_manager.input_dataframe_generator import set_table_in_survey
+#from hackathon_dsn.survey_scenario import config_files_directory
+from adbc_driver_postgresql import dbapi
 import os
+
+from simulation.survey_scenario import DsnSurveyScenario
 
 application = Flask(__name__)
 data = pd.read_hdf("/media/veracrypt3/test")
@@ -23,10 +28,114 @@ def fetch(siren):
     data[data.id_employeur == int(siren)].to_hdf(get_file_path(id), key="data")
     return jsonify({"siren": siren, "action": "fetch", "id": id})
 
+def prepare_data(id):
+    # postgres://[user]:[pwd]@10.0.0.1:5432/dsn
+    current_dir = os.getcwd()
+    with open(f"{current_dir}/db", 'r') as o:
+        constr = o.read()
+    conn = dbapi.connect(constr)
+    data = pd.read_sql("""select ids.*,
+	    vers_mai.remuneration_nette_fiscale mai,
+	    vers_avril.remuneration_nette_fiscale avril,
+	    vers_mars.remuneration_nette_fiscale mars
+	    from
+        (
+        select id_assure
+        from (
+        	select * from dadeh.ddadtversement where mois_declaration = '2023-05-01'
+        ) vers
+        inner join (
+        	select assoc.* from
+        		dadeh.ddadtemployeur emp
+        		inner join dadeh.ddadtemployeur_assure assoc on assoc.id_employeur = emp.id
+        		inner join dadeh.ddadtassure assur on assoc.id_assure = assur.id
+        --		where emp.en_code_apen = '7820Z'
+        	) assoc on assoc.id = vers.id_employeur_assure
+        group by id_assure
+        ) ids
+        left join (
+        	select id_assure, sum(remuneration_nette_fiscale) remuneration_nette_fiscale
+        	from dadeh.ddadtversement vv
+        	inner join dadeh.ddadtemployeur_assure assoc on assoc.id = vv.id_employeur_assure
+        	where mois_declaration = '2023-05-01'
+        	group by id_assure
+        	) vers_mai on ids.id_assure = vers_mai.id_assure
+        left join (
+        	select id_assure, sum(remuneration_nette_fiscale) remuneration_nette_fiscale
+        	from dadeh.ddadtversement vv
+        	inner join dadeh.ddadtemployeur_assure assoc on assoc.id = vv.id_employeur_assure
+        	where mois_declaration = '2023-04-01'
+        	group by id_assure
+        	) vers_avril on ids.id_assure = vers_avril.id_assure
+        left join (
+        	select id_assure, sum(remuneration_nette_fiscale) remuneration_nette_fiscale
+        	from dadeh.ddadtversement vv
+        	inner join dadeh.ddadtemployeur_assure assoc on assoc.id = vv.id_employeur_assure
+        	where mois_declaration = '2023-03-01'
+        	group by id_assure
+        	) vers_mars on ids.id_assure = vers_mars.id_assure""",conn)
+    data = data.reset_index().fillna(0)[['id_assure','mai','avril','mars']]
+    data['idfam'] = range(len(data))
+    data['idfoy'] = range(len(data))
+    data['idmen'] = range(len(data))
+    data['quifam'] = data.idfam * 0
+    data['quifoy'] = data.idfoy * 0
+    data['quimen'] = data.idmen * 0
+
+    config_files_directory = os.getcwd()
+    collection = "dsn"
+    survey_name = f"dsn_{id}_2023-05"
+    for period in ["2023-05", "2023-04", "2023-03"]:
+        if period == "2023-05":
+            individus = data.drop(["mars","avril"],axis = 1).rename(columns = {"mai":"salaire_net"})
+        elif period == "2023-04":
+            individus = data.drop(["mars","mai"],axis = 1).rename(columns = {"avril":"salaire_net"})
+        elif period == "2023-03":
+            individus = data.drop(["avril","mai"],axis = 1).rename(columns = {"mars":"salaire_net"})
+        individus = individus.fillna(0)
+        foyer_fiscaux = pd.DataFrame(individus)[['idfoy']]
+        familles = pd.DataFrame(individus)[['idfam']]
+        menages = pd.DataFrame(individus)[['idmen']]
+        set_table_in_survey(
+            individus,
+            entity = "individu",
+            period = period,
+            collection = collection,
+            survey_name = survey_name,
+            config_files_directory = config_files_directory
+            )
+
+        set_table_in_survey(
+            menages,
+            entity = "menage",
+            period = period,
+            collection = collection,
+            survey_name = survey_name,
+            config_files_directory = config_files_directory
+            )
+
+        set_table_in_survey(
+            familles,
+            entity = "famille",
+            period = period,
+            collection = collection,
+            survey_name = survey_name,
+            config_files_directory = config_files_directory
+            )
+
+        set_table_in_survey(
+            foyer_fiscaux,
+            entity = "foyer_fiscal",
+            period = period,
+            collection = collection,
+            survey_name = survey_name,
+            config_files_directory = config_files_directory
+            )
+
 
 @application.route("/entreprise/<siren>/compute/<id>")
 def compute(siren, id):
-    df = pd.read_hdf(get_file_path(id))
+    # df = pd.read_hdf(get_file_path(id))
     #     return [
     #   {
     #     nom: "Joe",
@@ -36,4 +145,10 @@ def compute(siren, id):
     #     ppa: 120,
     #   },
     # ];
-    return jsonify([{"nom": "Doe", "prenom": "John", "id": "jd", "email": "j@d.oe", "ppa": 123}])
+    # return jsonify([{"nom": "Doe", "prenom": "John", "id": "jd", "email": "j@d.oe", "ppa": 123}])
+    prepare_data(id)
+    simulation = DsnSurveyScenario(collection="dsn", survey_name=f"dsn_{id}_2023-05")
+    simul = simulation.simulations['baseline'].create_data_frame_by_entity(['ppa'],period = '2023-06')['famille']
+    simul = simul.loc[simul.ppa > 0]
+    return simul.to_json()
+
---
+++ simulation/survey_scenario.py
@@ -0,0 +1,96 @@
+import logging
+import os
+
+from openfisca_survey_manager.scenarios.abstract_scenario import AbstractSurveyScenario
+from openfisca_survey_manager.survey_collections import SurveyCollection
+from simulation.tax_benefits_system import create_tbs
+
+from simulation import root_path
+
+log = logging.getLogger(__name__)
+
+admissible_simulation_prefix = [
+    "baseline",
+]
+
+tax_benefit_system = create_tbs()
+
+config_files_directory = os.path.join(
+    root_path, ".config", "openfisca-survey-manager"
+)
+
+config_files_directory = '/home/cgl/leximpact/hackathon_dsn/hackathon_dsn/.config/openfisca-survey-manager/'
+
+input_variables = [
+    "idfam",
+    "idfoy",
+    "idmen",
+    "quifam",
+    "quifoy",
+    "quimen",
+    "salaire_net"
+]
+
+class DsnSurveyScenario(AbstractSurveyScenario):
+    """Survey scenario spécialisé pour l'ERFS-FPR utilisée par Leximpact."""
+
+    used_as_input_variables = input_variables
+    id_variable_by_entity_key = dict(
+        famille="idfam",
+        foyer_fiscal="idfoy",
+        menage="idmen",
+    )
+
+    role_variable_by_entity_key = dict(
+        famille="quifam",
+        foyer_fiscal="quifoy",
+        menage="quimen",
+    )
+
+    def __init__(
+        self,
+        config_files_directory: str = config_files_directory,
+        annee_donnees = '2023-05',
+        period = '2023-05',
+        tax_benefit_systems = {"baseline" : tax_benefit_system},
+        collection: str = "dsn",
+        survey_name: str = None,
+    ):
+        
+        self.collection = collection
+        self.annee_donnees = annee_donnees
+        self.period = period
+
+        self.set_tax_benefit_systems(
+            tax_benefit_systems=tax_benefit_systems,
+        )
+
+        if survey_name is None:
+            survey_name = f"{collection}_{annee_donnees}"
+
+        # Création de la base de données sur les périodes voulues
+        survey_collection = SurveyCollection.load(
+            collection=collection, config_files_directory=config_files_directory
+        )
+        survey = survey_collection.get_survey(survey_name)
+
+        data = {"input_data_table_by_entity_by_period": {}, "survey": survey_name}
+        data["config_files_directory"] = config_files_directory
+
+        for month in ["2023-05", "2023-04", "2023-03"]:
+            if data["input_data_table_by_entity_by_period"].get(month) is None:
+                data["input_data_table_by_entity_by_period"][month] = {}
+            for table_name, _ in survey.tables.items():
+                current_month = table_name[-7:]
+                entity = table_name[:-8]
+                if current_month == month:
+                    data["input_data_table_by_entity_by_period"][month][
+                        entity
+                    ] = table_name
+
+        self.data = data
+
+        self.init_from_data(
+            data=data,
+            rebuild_input_data=False,
+        )
